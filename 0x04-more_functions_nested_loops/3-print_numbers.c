0c2eb7d75b9b16892a6e9d88f70fc3a8de598e79
Kosisochukwu Iyiegbu <iyiegbuk@gmail.com>
2022-09-15 08:20:47 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints all numbers from
+ * 0 to 9
+ *
+ * Return: returns nothing
+ */
+
+void print_numbers(void)
+{
+	int num = 48;
+
+	for (num = 48; num < 58; num++)
+		_putchar(num);
+
+	_putchar(10);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - prints squares
+ * @size: size of the square
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < (size - 1); y++)
+				_putchar('#');
+
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+	return (0);
+}
