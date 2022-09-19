c54c2f640ccbaf3475cef41c9ecbdf7eadf32ac1
Kosisochukwu Iyiegbu <iyiegbuk@gmail.com>
2022-09-19 12:24:15 -0700

    fix for finals task

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,18 +10,24 @@
 
 int main(void)
 {
-	int trand;
-	int count;
-	int total;
+	int pass[100];
+	int i, sum, n;
 
+	sum = 0;
 	srand(time(NULL));
-	for (count = 0; total = 2772; total > 122; count++)
-	{
-		trand = (rand() % 125) + 1;
-		printf("%c", trand);
-		total -= trand;
-	}
-	printf("%c", total);
 
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 	return (0);
 }
