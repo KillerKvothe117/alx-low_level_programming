55a727afd0b1a56b2b4c7ec5b7c91ed70cd3154d
Kosisochukwu Iyiegbu <iyiegbuk@gmail.com>
2022-09-22 00:50:33 -0700

    tasks

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * infinite_add - adss two numbers
+ * @n1: First number
+ * @n2: Second number
+ * @r: result buffer
+ * @size_r: result buffer size
+ *
+ * Return: 0 if the result can not be stored in r,
+ * otherwise a pointer to the result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
